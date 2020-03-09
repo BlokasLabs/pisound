c3da09558e5b3ae58b766d93af09b78256d0bc57
Giedrius <giedrius@blokas.io>
2020-03-09 14:45:21 +0000

    Small updates to install scripts

--- pisound-config/scripts/install-audacity.sh
+++ pisound-config/scripts/install-audacity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 SOFTWARE_TO_INSTALL="audacity"
-sudo apt-get update
-sudo apt-get install $SOFTWARE_TO_INSTALL -y
+apt-get update
+apt-get install $SOFTWARE_TO_INSTALL -y
 echo "Done! Thank you!"
