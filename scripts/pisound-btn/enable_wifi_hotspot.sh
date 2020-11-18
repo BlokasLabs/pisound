9bb09b99a71b95086878293d292a7f6c02e56349
Giedrius <giedrius@blokas.io>
2020-11-18 13:08:17 +0000

    Remove sudo usage in WiFi enable/disable scripts. Skip disabling WiFi hotspot in case the system is stopping

--- scripts/pisound-btn/enable_wifi_hotspot.sh
+++ scripts/pisound-btn/enable_wifi_hotspot.sh
@@ -25,21 +25,21 @@ SCRIPT_PATH=$(dirname $(readlink -f $0))
 flash_leds 1
 log "Enabling Access point..."
 
-sudo rfkill unblock wifi
-sudo wpa_cli -i wlan0 disconnect
-sudo dhcpcd --denyinterfaces wlan0
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 172.24.1.1 netmask 255.255.255.0 broadcast 172.24.1.255
-sudo systemctl stop dnsmasq
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-sudo systemctl start dnsmasq
-(sleep 15 && sudo systemctl restart avahi-daemon) &
-sudo systemctl unmask hostapd
-sudo systemctl start hostapd
+rfkill unblock wifi
+wpa_cli -i wlan0 disconnect
+dhcpcd --denyinterfaces wlan0
+ifconfig wlan0 down
+ifconfig wlan0 172.24.1.1 netmask 255.255.255.0 broadcast 172.24.1.255
+systemctl stop dnsmasq
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+echo 1 > /proc/sys/net/ipv4/ip_forward
+systemctl start dnsmasq
+(sleep 15 && systemctl restart avahi-daemon) &
+systemctl unmask hostapd
+systemctl start hostapd
 
-sudo systemctl restart touchosc2midi 2>/dev/null
+systemctl restart touchosc2midi 2>/dev/null
 
 flash_leds 20
