d439530a4d1cf5f484432ea4b51d7c09899498bd
Giedrius <giedrius@blokas.io>
2024-01-31 09:49:39 +0000

    Pisound Meta package updated and restructured

--- pisound-meta/make_deb.sh
+++ pisound-meta/make_deb.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-#gzip --best -n ./debian/usr/share/doc/pisound-ctl/changelog ./debian/usr/share/doc/pisound-ctl/changelog.Debian ./debian/usr/share/man/man1/pisound-ctl.1
-fakeroot dpkg --build debian
-mv debian.deb pisound.deb
-#gunzip `find . | grep gz`
+dpkg-buildpackage -b
